5826b76deba3ed7040243b353733a850cce4ea65
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-22 21:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function volume_sphere() {
     //Write your code here
 	let r = document.getElementById("radius").value;
 	let v = document.getElementById("volume");
-
-	let vol = (4/3)(22/7)(r**3);
+	// r.value = "Rad";
+	let vol = (4/3)*(22/7)*(r**3);
 
 	v.value = vol;
 } 
